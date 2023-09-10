da989413f5da00b5e68f718d08756f51dc0a9f8c
yunusemrecftci <yunusemrecftci@gmail.com>
2023-09-10 12:30:24 +0300

    ex05

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void ft_putstr(char *str)
+    {   
+        int i=0;
+        while(str[i]){
+            ft_putchar(str[i]);
+            i++;
+        }
+        
+    }
+int main()
+{
+    ft_putstr("Hello");
+}
+

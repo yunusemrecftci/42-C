cfed70a6bcb20bb14e057d9242ba2ed3cb026588
yunusemrecftci <yunusemrecftci@gmail.com>
2023-09-11 23:42:25 +0300

    -

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -15,7 +15,7 @@ void ft_rev_int_tab(int *tab, int size)
         
     }
 int main()
-{   
+{    
     int number[]={1,9,2,8,3,8,4,8,4,5,76,45};
     ft_rev_int_tab(number,12);
     for (int i = 0; i < 12; i++ ){
